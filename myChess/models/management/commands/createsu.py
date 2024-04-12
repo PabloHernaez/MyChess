054e1fe613f3dd2c5416c3c6080329462750323c
PabloHernaez <pablohernaezdelgado@gmail.com>
2024-04-12 09:41:19 +0200

    Arreglo tonto

--- myChess/models/management/commands/createsu.py
+++ myChess/models/management/commands/createsu.py
@@ -5,7 +5,7 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
     help = 'creates a user'
 
-    def handler(self, *args, **options):
+    def handle(self, *args, **options):
         if not User.objects.filter(username='alumnodb').exists():
             User.objects.create_superuser(
                 username='alumnodb',
